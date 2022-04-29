0e0fdcb1def8b92c127b43139f5297a089e1548d
avmustafaev <alexander.mustafaev@gmail.com>
2022-04-29 15:46:02 +0500

    Py commit

---
+++ game_predict_number.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def number_predict(guessed_number: int = 1, minium_range: int = 1, maximum_range: int = 1000) -> int:
+    """Функция угадывает число за минимальное количество попыток
+
+    Args:
+        guessed_number (int, optional): Отгадываемое число. Defaults to 1.
+        minium_range (int, optional): Минимум диапазона. Defaults to 1.
+        maximum_range (int, optional): Максимум диапазона. Defaults to 1000.
+
+    Returns:
+        int: Возвращает количество попыток
+    """
+    predict_counter = 0
+    
+    while True:
+        predict_counter += 1
+        averaging = (minium_range+maximum_range) // 2
+
+        if averaging > guessed_number:
+            maximum_range = averaging
+
+        elif averaging < guessed_number:
+            minium_range = averaging
+
+        else:
+            print(
+                f"Число {guessed_number}: отгадано за {predict_counter} попыток.")
+            break  # отгадано: выход из цикла
+    return predict_counter
+
+if __name__ == "__main__":
+    # Если запускаем из тела скрипта, то выполняется:
+    min_range = 1
+    max_range = 2000
+    random_number = np.random.randint(min_range, max_range)
+    number_predict(random_number, min_range, max_range)
